bc06ad11d3ca7f261f97c1bf8b5c4f7fb1f5cd85
Yapnounkie <edmundopierre@yahoo.com>
2021-03-03 15:39:58 -0800

    fix minor errors

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,17 @@
 export function managePresents(state, action){
+
+  if (state ===undefined){
+      return state=[]
+  }
+    
+
+    switch(action.type){
+        case "INCREASE": 
+        return { numberOfPresents: state.numberOfPresents +1};
+
+        default:
+            return state;
+    }
+
+    
 }
